d2a5ac5dff59db3fd1364c42e90ec75a1e5aa6af
seanwutoronto <wusean@protonmail.com>
2021-09-07 13:01:11 -0400

    init

---
+++ Data Visualization/Data Visualization.R
@@ -0,0 +1,5 @@
+df = read.csv("C:\\Users\\Yggdrasil\\Desktop\\Programming\\Python\\Data Science\\Data Visualization\\Building_Permits.csv")
+
+dev.off()
+pairs(df, main = "Scatterplot Matrix")
+df[Proposed_Construction_Type]
